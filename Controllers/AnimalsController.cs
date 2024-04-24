fbb42b9f0d8b3d1684dc41812cc465a8fdf7ad8b
s27168 <s27168@pjwstk.edu.pl>
2024-04-24 22:36:38 +0200

    Initial commit

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,57 @@
+﻿using APBD6.Models;
+using APBD6.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD6.Controllers;
+
+[ApiController]
+[Route("animals")]
+public class AnimalsController : ControllerBase
+{
+
+    private IMockDb _mockDb;
+
+    public AnimalsController(IMockDb mockDb)
+    {
+        _mockDb = mockDb;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_mockDb.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetByID(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Animal animal)
+    {   
+        //Sprawdzenie czy ID nie jest zajete => 409 (Conflict)
+        if (_mockDb.GetAll().FirstOrDefault(e => e.ID == animal.ID) is not null)
+        {
+            return Conflict();
+        }
+        
+        _mockDb.Add(animal);
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Replace(Animal animal, int id)
+    {
+        _mockDb.Replace(animal, id);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _mockDb.Delete(id);
+        return NoContent();
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD6.Models;
+
+public class Animal
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public float Weight { get; set; }
+    public string Color { get; set; }
+}
